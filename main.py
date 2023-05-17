b776691b54879554428848bc323ad540971824d6
Bigman.Services <53261574+PasterSystems@users.noreply.github.com>
2023-05-17 15:07:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,83 @@
 import pygame
 import random
-# Määrab värvid
+#värvid RGB-väärtustena
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-# See määrab iga ruudustiku asukoha LAIUSE ja KÕRGUSE
+#Määrab ruudu laiuse ja kõrguse
 WIDTH = 20
 HEIGHT = 20
 
-# See määrab iga lahtri vahelise veerise
+#Määra ruudu servade vahe
 MARGIN = 5
 
-# Loo 2-mõõtmeline massiiv
+#Loob tühija ruudustik
 grid = []
 for row in range(10):
-        # Lisage tühi massiiv, mis mahutab iga lahtri
-        # selles reas
     grid.append([])
     for column in range(10):
-        grid[row].append(0)
-
-# genereerib 5 suvalist bommi
+        grid[row].append({'value': '', 'revealed': False})
+        
+#Genereerib juhuslikud pommitsoonid
 bombs = random.sample(range(100), 5)
 
+#Paigutab pommid ruudustikule
 for bomb in bombs:
     row = bomb // 10
     column = bomb % 10
-    grid[row][column] = 1
+    grid[row][column]['value'] = 'B'
 
-# alustab pygame
 pygame.init()
 
-# Määrab ekraani KÕRGUSE ja LAIUSE
+#Määrab akna suuruse
 WINDOW_SIZE = [255, 255]
 screen = pygame.display.set_mode(WINDOW_SIZE)
+#Määrab akna pealkiri
+pygame.display.set_caption("Minesweeper")
 
-# Määrab ekraani pealkirija
-pygame.display.set_caption("Array Backed Grid")
-
-# Korda, kuni kasutaja vajutab close nupule
 done = False
 
-# Kui kiiresti ekraan uuendab
 clock = pygame.time.Clock()
 
-# -------- Põhiprogrammi tsükkel -----------
+#Funktsioon, mis paljastab ruudustiku 
+def reveal_cell(row, column):
+    grid[row][column]['revealed'] = True
+    if grid[row][column]['value'] == 'B':
+        print("Sa vajutasid bommi peale!")
+    else:
+        print("Vajutasid tühja lahtri peale")
+
 while not done:
-    for event in pygame.event.get(): # Kasutaja tegi midagi
-        if event.type == pygame.QUIT: # Kui kasutaja vajutas close
-            done = True # Märgib et done ja lõpetab tsükkli
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Kasutaja vajutab hiirega et saada positsiooni
+            # Kui hiireklõps toimus, siis määra klõpsatud positsioon
             pos = pygame.mouse.get_pos()
-            # Muudab ekraani x/y koordinaadid ruudustiku koordinaatideks
             column = pos[0] // (WIDTH + MARGIN)
             row = pos[1] // (HEIGHT + MARGIN)
-            # Määrab selle asukohaks üks
-            grid[row][column] = 1
-            print("Click ", pos, "Grid coordinates: ", row, column)
+            #paljastab vastava ruudu
+            reveal_cell(row, column)
 
-    # Määrab ekraani tausta
+# Täidab ekraan musta värviga
     screen.fill(BLACK)
-    # Joonistab ruudustiku
+    
+# Joonista ruudustik ekraanile
     for row in range(10):
         for column in range(10):
             color = WHITE
-            if grid[row][column] == 1:
-                color = GREEN
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+            if grid[row][column]['revealed']:
+                if grid[row][column]['value'] == 'B':
+                    color = RED
+                else:
+                    color = GREEN
+            pygame.draw.rect(screen,color,[(MARGIN + WIDTH) * column + MARGIN,(MARGIN + HEIGHT) * row + MARGIN,WIDTH,HEIGHT])
 
-    # Limiit: 60 fps
+# Piirab fpsi 60 ticki sekundis
     clock.tick(60)
-    # Uuendab ekraani sellega mida teinud oleme
+# Kuvab ekraanile uuendatud kaater kui kasutaja vajutab gridi peale
     pygame.display.flip()
 
 pygame.quit()
